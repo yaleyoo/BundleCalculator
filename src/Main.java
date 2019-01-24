507cabdc642eb166c4e9618b11ea7e604dd13995
yaleyoo <steveguo1024@gmail.com>
2019-01-24 11:17:14 +1000

    弱智穷举 TODO:剔除switch

--- src/Main.java
+++ src/Main.java
@@ -52,12 +52,16 @@ public class Main {
 
     private static void printResult(int num, FmtCode fmt){
         double sum;
+        int bundle_10 = 0;
+        int bundle_9 = 0;
+        int bundle_5 = 0;
+        int bundle_3 = 0;
         switch (fmt){
             // IMG: 5 @ 450, 10 @ 800
             case IMG:
-                int bundle_10 = num / 10;
+                bundle_10 = num / 10;
                 int rest_10 = num % 10;
-                int bundle_5 = rest_10 / 5;
+                bundle_5 = rest_10 / 5;
                 sum = 800 * bundle_10 + 450 * bundle_5;
                 // print the result
                 System.out.println(num + " " + fmt + " $" + sum);
@@ -68,11 +72,11 @@ public class Main {
                 break;
             // Flac: 3 @ 427.5, 6 @ 810, 9 @ 1147.5
             case FLAC:
-                int bundle_9 = num / 9;
+                bundle_9 = num / 9;
                 int rest_9 = num % 9;
                 int bundle_6 = rest_9 / 6;
                 int rest_6 = rest_9 % 6;
-                int bundle_3 = rest_6 / 3;
+                bundle_3 = rest_6 / 3;
                 //int rest_3 = rest_6 % 3;
                 sum = bundle_9 * 1147.5 + bundle_6 * 810 + bundle_3 * 427.5;
                 // print the result
@@ -86,20 +90,43 @@ public class Main {
                 break;
             // VID: 3 @ 570, 5 @ 900, 9 @ 1530
             case VID:
-                int bundle_9_vid = num / 9;
-                int rest_9_vid = num % 9;
-                int bundle_5_vid = rest_9_vid / 5;
-                int rest_5_vid = rest_9_vid % 5;
-                int bundle_3_vid = rest_5_vid / 3;
-                sum = bundle_9_vid * 1530 + bundle_5_vid * 900 + bundle_3_vid * 570;
+                int min = 999;
+                int optimal = 0;
+                // 从0遍历到n/2+1，找到最理想（余数最小）的分割，穷举
+                for (int i=0;i<num/2+1;i++){
+                    if (Math.min(i%3, i%5) + Math.min((num - i) % 3, (num - i) % 5) == 0){
+                        optimal = i;
+                        break;
+                    }
+                    if (Math.min(i%3, i%5) + Math.min((num - i) % 3, (num - i) % 5) < min){
+                        optimal = i;
+                    }
+                }
+                // 分割左部
+                if (optimal%3 < optimal%5)
+                    bundle_3 = optimal/3;
+                else
+                    bundle_5 = optimal/5;
+                // 分割右部
+                if ((num-optimal)%3 < (num-optimal)%5)
+                    bundle_3 += (num-optimal)/3;
+                else
+                    bundle_5 += (num-optimal)/5;
+                // 整合， 9*5 = 9*5， 3*3=1*9
+                bundle_9 = (int)(bundle_5 / 9) * 5;
+                bundle_5 = (bundle_5 * 5 - bundle_9 * 9) / 5;
+                bundle_9 += bundle_3 / 3;
+                bundle_3 %= 3;
+
+                sum = bundle_9 * 1530 + bundle_5 * 900 + bundle_3 * 570;
                 // print the result
                 System.out.println(num + " " + fmt + " $" + sum);
-                if (bundle_9_vid!=0)
-                    System.out.println("    " + bundle_9_vid + " x 9 $1530");
-                if (bundle_5_vid!=0)
-                    System.out.println("    " + bundle_5_vid + " x 5 $900");
-                if (bundle_3_vid!=0)
-                    System.out.println("    " + bundle_3_vid + " x 3 $570");
+                if (bundle_9!=0)
+                    System.out.println("    " + bundle_9 + " x 9 $1530");
+                if (bundle_5!=0)
+                    System.out.println("    " + bundle_5 + " x 5 $900");
+                if (bundle_3!=0)
+                    System.out.println("    " + bundle_3 + " x 3 $570");
                 break;
         }
     }
