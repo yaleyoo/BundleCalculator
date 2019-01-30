cc867e8c357ce71ed09c6684b3a85650ac845252
yaleyoo <steveguo1024@gmail.com>
2019-01-30 18:59:14 +1100

    Working on Processor

--- src/model/ResultItem.java
+++ src/model/ResultItem.java
@@ -1,7 +1,34 @@
 package model;
 
+import java.util.Map;
+
 public class ResultItem {
     private OrderItem orderItem;
     private double sum;
+    private Map<Bundle, Integer> detail;
 
+
+    public OrderItem getOrderItem() {
+        return orderItem;
+    }
+
+    public void setOrderItem(OrderItem orderItem) {
+        this.orderItem = orderItem;
+    }
+
+    public double getSum() {
+        return sum;
+    }
+
+    public void setSum(double sum) {
+        this.sum = sum;
+    }
+
+    public Map<Bundle, Integer> getDetail() {
+        return detail;
+    }
+
+    public void setDetail(Map<Bundle, Integer> detail) {
+        this.detail = detail;
+    }
 }
--- src/utils/Processor.java
+++ src/utils/Processor.java
@@ -1,11 +1,25 @@
 package utils;
 
-import model.Order;
-import model.Result;
+import config.AvailableBundles;
+import model.*;
+
+import java.util.Comparator;
+import java.util.List;
 
 public class Processor {
 
     public static Result process(Order order){
-        return new Result();
+        Result result = new Result();
+        order.getItemList().forEach(item -> result.add(processItem(item)));
+
+        return result;
     }
+
+    public static ResultItem processItem(OrderItem item){
+        List<Bundle> bundleList = AvailableBundles.availableBundles.get(item.getFormatCode())
+                .stream()
+                .sorted(b -> b.getSize());
+        return new ResultItem();
+    }
+
 }
